4e74eb5454d7566d9843e5cd4bf51ff398d22708
MaryYakushik <mary.yakushik@gmail.com>
2016-02-27 20:51:37 +0300

    newproject

---
+++ desktop/src/my/coursework/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package my.coursework.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.coursework.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
